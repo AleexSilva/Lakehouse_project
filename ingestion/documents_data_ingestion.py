95dd4c4461c49a55b161807e1cdec0bdae3003f3
AleexSilva <silva.alexis94@gmail.com>
2026-03-28 15:49:36 +0000

    Doc data ingestion - v2

--- ingestion/documents_data_ingestion.py
+++ ingestion/documents_data_ingestion.py
@@ -2,3 +2,11 @@ import os
 import logging
 import boto3
 from ingestion.utils.file_path_manager import FilePathManager
+
+class DocumentDataIngestor:
+    def __init__(self, s3_bucket: str = None, aws_access_key_id: str = None, aws_secret_access_key: str = None, aws_s3_endpoint: str = None):
+        self.s3_bucket = s3_bucket
+        self.s3_client = boto3.client('s3', 
+                                      aws_access_key_id=aws_access_key_id, 
+                                      aws_secret_access_key=aws_secret_access_key,
+                                      endpoint_url=aws_s3_endpoint)
