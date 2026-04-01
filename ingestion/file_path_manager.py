108c4b9b49be666dec704c91805bf441558fac32
AleexSilva <silva.alexis94@gmail.com>
2026-04-01 02:06:16 +0000

    Filepath v.1

---
+++ ingestion/file_path_manager.py
@@ -0,0 +1,8 @@
+import os
+
+class FilePathManager:
+    def __init__(self, base_data_dir, lakehouse_s3_path):
+        self.base_data_dir = base_data_dir
+        self.lakehouse_s3_path = lakehouse_s3_path
+
+
