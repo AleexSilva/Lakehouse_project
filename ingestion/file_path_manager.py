71728033edb1580ff9285e7116eb890c414d74de
AleexSilva <silva.alexis94@gmail.com>
2026-04-01 02:07:44 +0000

    local_files

--- ingestion/file_path_manager.py
+++ ingestion/file_path_manager.py
@@ -5,4 +5,6 @@ class FilePathManager:
         self.base_data_dir = base_data_dir
         self.lakehouse_s3_path = lakehouse_s3_path
 
-
+    def get_local_file_path(self, dataset_name, file_type):
+        file_name = f"{dataset_name}.{file_type}"
+        return os.path.join(self.base_data_dir, file_name)
