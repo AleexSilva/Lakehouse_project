79869618d38578c95aadb5347f02e7bd7e2687fb
AleexSilva <silva.alexis94@gmail.com>
2026-04-01 02:12:40 +0000

    chargenet_ingest ingest function v1

--- ingestion/bronze/chargenet_ingest.py
+++ ingestion/bronze/chargenet_ingest.py
@@ -3,3 +3,11 @@ from ingestion.utils.data_ingestor import DataIngestor
 from ingestion.utils.config_loader import ConfigLoader
 from ingestion.utils.file_path_manager import FilePathManager
 
+def ingest_chargenet():
+    config = ConfigLoader()
+    spark = create_spark_session()
+    ingestor = DataIngestor(spark)
+    
+
+if __name__ == "__main__":
+    ingest_chargenet()
