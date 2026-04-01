b1e7819d507c96b2264159e96e439be43b90a0a5
AleexSilva <silva.alexis94@gmail.com>
2026-04-01 02:13:06 +0000

    chargenet_ingest ingest function v2

--- ingestion/bronze/chargenet_ingest.py
+++ ingestion/bronze/chargenet_ingest.py
@@ -7,7 +7,13 @@ def ingest_chargenet():
     config = ConfigLoader()
     spark = create_spark_session()
     ingestor = DataIngestor(spark)
-    
+    path_manager = FilePathManager(config.base_data_dir, config.lakehouse_s3_path)
+
+    stations_file = path_manager.get_local_file_path("chargenet_stations", "json")
+    ingestor.ingest_file_to_bronze(stations_file, "chargenet", "stations", "json")
+
+    sessions_file = path_manager.get_local_file_path("chargenet_charging_sessions", "json")
+    ingestor.ingest_file_to_bronze(sessions_file, "chargenet", "charging_sessions", "json")
 
 if __name__ == "__main__":
     ingest_chargenet()
