5ac298d187c8d9481347580ecf4e2e8bc08cd8fb
AleexSilva <silva.alexis94@gmail.com>
2026-04-07 02:22:20 +0000

    [ingestion] document s3 function

--- ingestion/bronze/documents_ingest.py
+++ ingestion/bronze/documents_ingest.py
@@ -3,3 +3,9 @@ from ingestion.utils.config_loader import ConfigLoader
 from ingestion.utils.file_path_manager import FilePathManager
 from ingestion.utils.document_data_ingestor import DocumentDataIngestor
 import os
+
+
+def format_s3_bucket_name(s3_path):
+    if s3_path.startswith("s3a://"):
+        s3_path = s3_path[6:]
+    return s3_path.rstrip('/')
