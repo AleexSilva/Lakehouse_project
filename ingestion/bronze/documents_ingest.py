4bff46d8ca03c175843d1a4081bb43b9e309870b
AleexSilva <silva.alexis94@gmail.com>
2026-04-07 02:23:42 +0000

    [Ingestion] ingest docs function v1

--- ingestion/bronze/documents_ingest.py
+++ ingestion/bronze/documents_ingest.py
@@ -9,3 +9,12 @@ def format_s3_bucket_name(s3_path):
     if s3_path.startswith("s3a://"):
         s3_path = s3_path[6:]
     return s3_path.rstrip('/')
+
+def ingest_documents():
+    config = ConfigLoader()
+    ingestor = DocumentDataIngestor(
+        s3_bucket=format_s3_bucket_name(config.lakehouse_s3_path),
+        aws_access_key_id=config.aws_access_key_id,
+        aws_secret_access_key=config.aws_secret_access_key,
+        aws_s3_endpoint=config.aws_s3_endpoint
+    )
