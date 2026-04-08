8ae46ca013b53c953bb5a343c46617cf83b380af
AleexSilva <silva.alexis94@gmail.com>
2026-04-08 03:16:57 +0000

    [Ingestion] ecoride ingestion file v.2

--- ingestion/bronze/ecoride_ingrest.py
+++ ingestion/bronze/ecoride_ingrest.py
@@ -2,3 +2,10 @@ from ingestion.utils.spark_utils import create_spark_session
 from ingestion.utils.config_loader import ConfigLoader
 from ingestion.utils.file_path_manager import FilePathManager
 from ingestion.utils.data_ingestor import DataIngestor
+
+
+def ingest_ecoride():
+    pass
+
+if __name__ == "__main__":
+    ingest_ecoride()
