2daac75f2a1332fdefca0e1072b0efb90c7dd974
AleexSilva <silva.alexis94@gmail.com>
2026-04-08 03:18:31 +0000

    [Ingestion] ecoride ingestion fle v.4 testing the first file ingestion

--- ingestion/bronze/ecoride_ingrest.py
+++ ingestion/bronze/ecoride_ingrest.py
@@ -10,5 +10,9 @@ def ingest_ecoride():
     ingestor = DataIngestor(spark)
     path_manager = FilePathManager(config.base_data_dir, config.lakehouse_s3_path)
 
+    customer_file = path_manager.get_local_file_path("ecoride_customers", "csv")
+    ingestor.ingest_file_to_bronze(customer_file, "ecoride", "customers", "csv")
+
+
 if __name__ == "__main__":
     ingest_ecoride()
